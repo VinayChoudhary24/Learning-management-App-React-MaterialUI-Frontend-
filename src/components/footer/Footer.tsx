11c8bddc79af14f93901766c4d83018fbf3e9c48
vinaychoudhary24 <vinaychoudhary994@gmail.com>
2025-10-22 14:51:28 +0530

    updated/portfolio/link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -66,7 +66,7 @@ export default function Footer() {
             </Typography>
             <Box display="flex" flexDirection="column" gap={1}>
               <Link
-                href="https://vinay-choudhary-portfolio-nextjs.vercel.app/"
+                href="https://vinay-choudhary.vercel.app/"
                 color="inherit"
                 underline="hover"
                 target="_blank"
